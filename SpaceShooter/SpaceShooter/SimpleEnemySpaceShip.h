765e5edcbb5e311906b2585d443d277893bf1d7b
Kristian Skarseth <kristianskarseth@msn.com>
2012-09-27 22:20:12 +0200

    started on simpleEnemySpaceShip class

---
+++ SpaceShooter/SpaceShooter/SimpleEnemySpaceShip.h
@@ -0,0 +1,27 @@
+/********************************************************************
+*	Created:	2012/09/27  21:39
+*	Filename: 	SimpleEnemySpaceShip.h
+*	Author:		Kristian Skarseth
+*	
+*	purpose:	
+*********************************************************************/
+
+#ifndef SimpleEnemySpaceShip_H
+#define SimpleEnemySpaceShip_H
+
+
+#endif // SimpleEnemySpaceShip_H
+#include "SpaceShipBase.h"
+
+class CSimpleEnemySpaceShip : public CSpaceShip
+{
+
+public:
+	CSimpleEnemySpaceShip();
+	~CSimpleEnemySpaceShip();
+
+protected:
+
+private:
+
+};
