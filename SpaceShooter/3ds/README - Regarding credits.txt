820216a67076bf5de7e2781ae1b8cefbd7e67ccf
Kristian Skarseth <kristianskarseth@msn.com>
2012-11-05 19:22:00 +0100

    Finally removed old version of the game

--- SpaceShooter/3ds/README - Regarding credits.txt
+++
@@ -1,5 +0,0 @@
-None of the 3ds models here are made by me. I have only done small changes to some of them.
-All the models have been downloaded from http://www.scifi3d.com/, and all credits for the 
-models goes to the original creators.
-
-For individual credits see the attached readme file inside the spesific model folder. 
--- SpaceShooter/SpaceShooter/LogSys.h
+++
@@ -1,42 +0,0 @@
-/********************************************************************
-	created:	2012/01
-	filename: 	LogSys.h
-
-	author:		Kristian Skarseth, Peder Thorup, Kjetil Raaen
-	
-	purpose:	Logger class. This class is a singleton which gets the 
-				log lines from the Logger class and takes care of writing
-				them to file or console.
-*********************************************************************/
-
-#ifndef LogSys_H
-#define LogSys_H
-
-#include <string>
-#include <fstream>
-enum Level{
-	INFO,WARN,ERRORX,CRITICAL
-};
-/*
-Main logsystem class. All calls will be direcetd through this singleton.
-*/
-class LogSys
-{
-public: 
-	static LogSys& Instance();
-	void Write(const std::string &source,Level level,const std::string &message);
-private:  LogSys(const std::string &fileName); // ctor hidden  
-		   LogSys(LogSys const&); // copy ctor hidden  
-		   LogSys& operator=(LogSys const&); // assign op. hidden  
-		   ~LogSys(); // dtor hidden
-		   static const std::string SEPARATOR;
-		   bool toConsole; // set to false to stop logging to console.
-		   std::fstream fileOut;
-};
-
-;
-static std::string levelNames[] = {
-	"Info", "Warn", "Error", "Critical" 
-};
-
-#endif // LogSys_H
